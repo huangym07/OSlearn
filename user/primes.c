2fb08d8479c8cd283effebdb5c5493aab3dd33a4
huangym07 <987297028@qq.com>
2024-05-19 22:56:54 +0800

    recursion version for util lab primes
    之前的版本,我以为可能会出现父进程占据 CPU 较久的时间向管道 write
    好多数,之后 CPU
    运行子进程读取的时候只能读取到最后一个,我以为管道只能存储一个
    int,所以父进程 read 到子进程向它 write 一个信号之后才向子进程
    write,所以写的异常复杂,实际上不用.

--- user/primes.c
+++ user/primes.c
@@ -1,80 +1,54 @@
+// 递归实现
 #include "kernel/types.h"
 #include "user/user.h"
 
+void new_proc(int p[]) {
+  close(p[1]);
+  int pri = -1;
+  if (read(p[0], &pri, 4) != 4) {
+    fprintf(1, "child process failed to read prime from the pipe\n");
+    exit(1);
+  }
+  fprintf(1, "prime %d\n", pri);
+
+  int num = 0, child = 0, newp[2];
+  while (read(p[0], &num, 4)) {
+    if (num % pri == 0) continue;
+    if (!child) {
+      pipe(newp);
+      if (fork() == 0) new_proc(newp);
+      else close(newp[0]);
+      child = 1;
+    } 
+    write(newp[1], &num, 4);
+  }
+
+  close(p[0]);
+  close(newp[1]);
+  wait(0);
+  exit(0);
+
+}
+
 int main()
 {
-  close(0);
-  int p1[2], p2[2], p3[2], p4[2];
-  char buf[] = {'a'};
-  pipe(p1);
-  pipe(p2);
-  int pid = fork();
-  if (pid == 0) {
-    close(p1[1]);
-    close(p2[0]);
-    write(p2[1], buf, 1);
-    int pri = -1, child = 0; // child: whether it has child
-    int num;
-    while (read(p1[0], &num, 4)) {
-      if (pri == -1) {
-        pri = num;
-        fprintf(1, "prime %d\n", pri);
-      }
-      if (num % pri == 0) {
-        write(p2[1], buf, 1);
-        continue;
-      }
-      if (!child) {
-        child = 1;
-        pipe(p3);
-        pipe(p4);
-        pid = fork();
-        if (pid == 0) {
-          close(p1[0]);
-          close(p2[1]);
-          p1[0] = p3[0], p1[1] = p3[1];
-          p2[0] = p4[0], p2[1] = p4[1];
-          close(p1[1]);
-          close(p2[0]);
-          pri = -1, child = 0;
-          write(p2[1], buf, 1);
-          read(p1[0], &num, 4);
-          pri = num;
-          fprintf(1, "prime %d\n", pri);
-        } else {
-          close(p3[0]);
-          close(p4[1]);
-          if (read(p4[0], buf, 1)) write(p3[1], &num, 4);
-        }
-      } else {
-        if (read(p4[0], buf, 1)) write(p3[1], &num, 4);
-      }
-      write(p2[1], buf, 1);
-    }
-    if (!child) {
-      close(p1[0]);
-      close(p2[1]);
-      exit(0);
-    } else {
-      close(p3[1]);
-      read(p4[0], buf, 1);
-      close(p4[0]);
-      close(p1[0]);
-      close(p2[1]);
-      exit(0);
-    }
+  int p[2];
+  pipe(p);
+
+  if (fork() == 0) {
+    new_proc(p);
   } else {
-    close(p1[0]);
-    close(p2[1]);
+    close(p[0]);
     for (int i = 2; i <= 35; i++) {
-      read(p2[0], buf, 1);
-      write(p1[1], &i, 4);
+      if (write(p[1], &i, 4) != 4) {
+        fprintf(1, "first process failed to write %d into the pipe\n", i);
+        exit(1);
+      }
     }
-    close(p1[1]);
-    read(p2[0], buf, 1);
-    close(p2[0]);
+    close(p[1]);
+    wait(0);
     exit(0);
   }
 
-  exit(0);
+  return 0;
 }
