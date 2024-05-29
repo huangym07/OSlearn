09a6f7e025c363b25586fd2050f15174615fad73
huangym07 <987297028@qq.com>
2024-05-29 20:16:03 +0800

    my solution for lab pgtbl pte printout

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,21 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// print recursively contents in the root-page-table
+void vmprint(pagetable_t pagetable, int level)
+{
+  if (level == 1) 
+    printf("page table %p\n", pagetable);
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      for (int j = 0; j < level; j++) {
+        printf("..");
+        if (j < level - 1) printf(" ");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      if (level < 3) vmprint((pagetable_t)PTE2PA(pte), level + 1);
+    }
+  }
+} 
